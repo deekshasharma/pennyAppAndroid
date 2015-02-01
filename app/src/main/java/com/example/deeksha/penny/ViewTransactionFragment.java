99614cd4bf4522ff6df7f059340f20f721ced197
Deeksha Sharma <deeksha.sharma25@gmail.com>
2015-01-31 23:21:23 -0800

    view transactions header

--- app/src/main/java/com/example/deeksha/penny/ViewTransactionFragment.java
+++ app/src/main/java/com/example/deeksha/penny/ViewTransactionFragment.java
@@ -6,11 +6,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class ViewTransactionFragment extends Fragment {
 
     private static final String POSITION = "position";
+    private TextView getYear;
+    private TextView getMonths;
+    private TextView getTransactions;
 
     public ViewTransactionFragment(){}
 
@@ -28,7 +33,18 @@ public class ViewTransactionFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_view_transaction, container, false);
+
+        View view = getLayoutInflater(savedInstanceState).inflate(R.layout.fragment_view_transaction, container, false);
+
+        getYear = (TextView) view.findViewById(R.id.getYear);
+        getMonths = (TextView) view.findViewById(R.id.getMonths);
+        getTransactions = (TextView) view.findViewById(R.id.getTransactions);
+        getYear.setText("2015");
+        getMonths.setText("JAN");
+        getTransactions.setText("40");
+        return view;
+
+//        return inflater.inflate(R.layout.fragment_view_transaction, container, false);
     }
 
     @Override
